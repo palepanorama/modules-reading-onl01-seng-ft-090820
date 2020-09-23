8ff2c9aecf2a5ffc5d255aae163e4c95742726aa
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 23:48:13 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,9 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  extend 
-    Metadancing 
-  end 
+  extend Metandcing 
   include Dance 
   
   attr_accessor :name 
