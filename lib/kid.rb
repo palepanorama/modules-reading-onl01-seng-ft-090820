9fbccd6bd14e3a85efa2e371361380f1ab5b0a64
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 23:48:40 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,14 +2,5 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend 
-    MetaDancing 
-  end 
-  include Dance 
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end 
+  extend MetaDancing 
 end 
